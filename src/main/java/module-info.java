baef41aa0af5fa4cbda6905ee3de60b0828b4d72
Quentin Rocher <qrocher@weyield.io>
2025-05-16 13:28:50 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module fr.isep.edt_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens fr.isep.edt_project to javafx.fxml;
+    exports fr.isep.edt_project;
+}
