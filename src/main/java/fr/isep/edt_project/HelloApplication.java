c2eec176648d92e8ef5678f021e187647004cf6f
Quentin Rocher <qrocher@weyield.io>
2025-05-19 11:07:38 +0200

    renomage du titre de l'application

--- src/main/java/fr/isep/edt_project/HelloApplication.java
+++ src/main/java/fr/isep/edt_project/HelloApplication.java
@@ -1,18 +1,18 @@
 package fr.isep.edt_project;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Application de gestion d'emploi du temps!");
         stage.setScene(scene);
         stage.show();
     }
