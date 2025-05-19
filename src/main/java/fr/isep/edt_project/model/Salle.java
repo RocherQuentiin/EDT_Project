0ab5ee8f92b99e7ec578ed55dfb83ecb000799e6
angel <angeline.chapuis@eleve.isep.fr>
2025-05-19 13:47:26 +0200

    getter et setter pour la classe salle

--- src/main/java/fr/isep/edt_project/model/Salle.java
+++ src/main/java/fr/isep/edt_project/model/Salle.java
@@ -6,7 +6,30 @@ public class Salle {
     private int capacite;
     private String localisation;
 
-    public boolean estDisponible() { return true; }
-    public String afficherInfos() { return ""; }
-    public boolean aConflict() { return false; }
+    public Salle(int id, String numeroSalle, int capacite, String localisation) {
+        this.id = id;
+        this.numeroSalle = numeroSalle;
+        this.capacite = capacite;
+        this.localisation = localisation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNumeroSalle() {
+        return numeroSalle;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public boolean estDisponible(Horaire h) { return true; }
+    public String afficherInfos() { return numeroSalle + " (" + capacite + " places)"; }
+    public boolean aConflict(Horaire h) { return false; }
 }
