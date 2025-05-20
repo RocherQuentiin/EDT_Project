2dbfc7369e6f5cf264b530f0a31def3c9716d95f
angel <angeline.chapuis@eleve.isep.fr>
2025-05-20 23:13:20 +0200

    mise à jour pour afficher le calendrier dans la classe controller principale

--- src/main/java/fr/isep/edt_project/controller/Controller.java
+++ src/main/java/fr/isep/edt_project/controller/Controller.java
@@ -9,10 +9,21 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Controller {
+
     public static void changeScene(Stage stage, String fxmlPath) throws IOException {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(fxmlPath));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
+    }
+
+    // Ma méthode générique qui retourne le contrôleur pour ma implémenter mon calendrier
+    protected <T> T changeSceneAndGetController(Stage stage, String fxmlPath, int width, int height, String title) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(fxmlPath));
+        Scene scene = new Scene(loader.load(), width, height);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
+        return loader.getController();
     }
 
     void showAlert(Alert.AlertType type, String title, String message) {
