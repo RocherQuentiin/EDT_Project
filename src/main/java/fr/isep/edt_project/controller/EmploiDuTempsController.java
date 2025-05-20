e00043ea7c06a7fb98c912c7e6ad83ec2f390d7d
angel <angeline.chapuis@eleve.isep.fr>
2025-05-20 22:11:39 +0200

    implémentation du calendrier vide avec CalendarFX dans EmploiDuTempsController

--- src/main/java/fr/isep/edt_project/controller/EmploiDuTempsController.java
+++ src/main/java/fr/isep/edt_project/controller/EmploiDuTempsController.java
@@ -1,4 +1,35 @@
 package fr.isep.edt_project.controller;
 
-public class EmploiDuTempsController {
+import com.calendarfx.view.CalendarView;
+import com.calendarfx.model.CalendarSource;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+public class EmploiDuTempsController implements Initializable {
+
+    @FXML
+    private BorderPane rootPane;
+
+    private CalendarView calendarView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        calendarView = new CalendarView();
+
+        calendarView.setToday(LocalDate.now());
+        calendarView.setTime(LocalTime.now());
+        calendarView.setShowAddCalendarButton(false);
+        calendarView.setShowPrintButton(false);
+
+        CalendarSource source = new CalendarSource("EDT ISEP");
+        calendarView.getCalendarSources().add(source);
+
+        rootPane.setCenter(calendarView);
+    }
 }
