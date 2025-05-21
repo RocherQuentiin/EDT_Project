4a88ce5943653c820a03df78cfb912b843b67b1f
angel <angeline.chapuis@eleve.isep.fr>
2025-05-21 21:39:03 +0200

    Connexion utilisateur fonctionnelle - Vérification du mot de passe avec la BDD - Initialisation correcte de l’utilisateur courant dans Session - Redirection vers HomeController + affichage du calendrier

--- src/main/java/fr/isep/edt_project/controller/LoginController.java
+++ src/main/java/fr/isep/edt_project/controller/LoginController.java
@@ -35,30 +35,28 @@ public class LoginController extends Controller{
         String email = loginField.getText();
         String password = passwordField.getText();
 
-        // Ici, tu peux instancier une classe concrète de Utilisateur (ex: Etudiant, Enseignant, etc.)
-        // Exemple simple avec une classe anonyme pour la démo :
-        Utilisateur utilisateur = new Utilisateur() {};
+        Utilisateur utilisateur = Utilisateur.getUserByEmail(email);
 
-        boolean success = utilisateur.seConnecter(email, password);
-        utilisateur = utilisateur.getUserByEmail(email);
-
-        if (success) {
+        if (utilisateur != null && utilisateur.seConnecter(email, password)) {
             Session.setUtilisateurCourant(utilisateur);
-            showAlert(Alert.AlertType.INFORMATION, "Connexion réussie", "Bienvenue " + utilisateur.getNom() + "!");
-        try{
-            Stage stage = (Stage) loginButton.getScene().getWindow();
-            HomeController controller = changeSceneAndGetController(
-                    stage,
-                    "/fr/isep/edt_project/home-view.fxml",
-                    1000, 700,
-                    "Accueil"
-            );
-            controller.showCalendarView(); //cette méthode permet d'afficher le calendrier
 
-        } catch (IOException e) {
-            showAlert(Alert.AlertType.ERROR, "Erreur", "Impossible de revenir à la fenêtre d'authentification !");
-        }
-            // TODO : Rediriger vers la page principale selon le type d'utilisateur
+            System.out.println("Utilisateur courant = " + utilisateur.getNom());
+
+            showAlert(Alert.AlertType.INFORMATION, "Connexion réussie", "Bienvenue " + utilisateur.getNom() + "!");
+
+            try {
+                Stage stage = (Stage) loginButton.getScene().getWindow();
+                HomeController controller = changeSceneAndGetController(
+                        stage,
+                        "/fr/isep/edt_project/home-view.fxml",
+                        1000, 700,
+                        "Accueil"
+                );
+                controller.showCalendarView(); // Affiche le calendrier
+
+            } catch (IOException e) {
+                showAlert(Alert.AlertType.ERROR, "Erreur", "Impossible de revenir à la fenêtre d'authentification !");
+            }
         } else {
             showAlert(Alert.AlertType.ERROR, "Erreur de connexion", "Email ou mot de passe incorrect.");
         }
